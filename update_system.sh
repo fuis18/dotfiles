1790b20036ceb1f3a7c9edf61d19748867c64c9a
Fuis18 <96286214+fuis18@users.noreply.github.com>
2026-04-22 22:15:57 -0500

    Add files via upload

---
+++ update_system.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Handle Errors
+set -euo pipefail
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Use sudo, need the root"
+    exit 1
+fi
+
+USER_NAME=$(logname)
+USER_HOME="/home/${USER_NAME}"
+USER_REPOS="${USER_HOME}/Downloads/repos"
+FUIS_REPO="${USER_REPOS}/fuis18/dotfiles"
+
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
+echo ""
+echo -e "${BLUE} ================================="
+echo -e "${GREEN} ======= Copiying My Files ======="
+echo -e "${BLUE} ================================="
+echo -e "${RESET}"
+
+# .config
+sudo -u "$USER_NAME" cp -r "${FUIS_REPO}/config/." "${USER_HOME}/.config/"
+
+sudo -u "$USER_NAME" cp -r "${FUIS_REPO}/zshrc" "${USER_HOME}/"
+mv "${USER_HOME}/zshrc" "${USER_HOME}/.zshrc"
+
+find "${USER_HOME}/.config/hypr/scripts/" -type f -name "*.sh" -exec chmod +x {} \;
