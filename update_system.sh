e841c40f9145bde36a26d685ef66730a653123e8
Fuis18 <96286214+fuis18@users.noreply.github.com>
2026-04-22 23:01:23 -0500

    Update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -25,8 +25,6 @@ echo -e "${RESET}"
 
 # .config
 sudo -u "$USER_NAME" cp -r "${FUIS_REPO}/.config/." "${USER_HOME}/.config/"
-
-sudo -u "$USER_NAME" cp -r "${FUIS_REPO}/.zshrc" "${USER_HOME}/"
-mv "${USER_HOME}/.zshrc" "${USER_HOME}/.zshrc"
+sudo -u "$USER_NAME" cp -r "${FUIS_REPO}/.zshrc" "${USER_HOME}/.zshrc"
 
 find "${USER_HOME}/.config/hypr/scripts/" -type f -name "*.sh" -exec chmod +x {} \;
