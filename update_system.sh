3a41055cf80bed54e56659e39b5ff2a6e5e9792c
Fuis18 <96286214+fuis18@users.noreply.github.com>
2026-04-22 20:32:39 -0500

    Update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+# Handle Errors
+set -euo pipefail
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Use sudo, need the root"
+    exit 1
+fi
+
+USER_NAME=$(logname)
+USER_HOME="/home/${USER_NAME}"
+USER_REPOS="${USER_HOME}/Downloads/repos"
+FUIS_REPO="${USER_REPOS}/fuis18/dotfiles"
+
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RESET='\033[0m'
+
 echo ""
 echo -e "${BLUE} ================================="
 echo -e "${GREEN} ======= Copiying My Files ======="
