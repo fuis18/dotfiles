553068a14ed8abbfeca7285109a9d2572c3524ed
fuis18 <warfuis18larcz018@gmail.com>
2026-07-10 08:49:47 -0500

    Fix error from nvim
    Fix tree files

--- .config/nvim/lua/plugins/illuminate.lua
+++ .config/nvim/lua/plugins/illuminate.lua
@@ -1,11 +1,13 @@
 return {
   "RRethy/vim-illuminate",
   event = { "BufReadPost", "BufNewFile" },
-  opts = {
-    delay = 200,
-    large_file_cutoff = 2000,
-    large_file_overrides = {
-      providers = { "lsp" },
-    },
-  },
+  config = function()
+    require("illuminate").configure({
+      delay = 200,
+      large_file_cutoff = 2000,
+      large_file_overrides = {
+        providers = { "lsp" },
+      },
+    })
+  end,
 }
