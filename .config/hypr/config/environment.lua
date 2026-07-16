6e674a78a87289dc2595a1056043710178033981
fuis18 <warfuis18larcz018@gmail.com>
2026-07-16 17:15:00 -0500

    Fix env compatibility driver steam
    Add x11 compatibility name

--- .config/hypr/config/environment.lua
+++ .config/hypr/config/environment.lua
@@ -8,7 +8,7 @@ hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
 hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
 hl.env("WEBRTC_ENABLE_HW_ENCODING", "1")
 hl.env("WEBRTC_ENABLE_HW_DECODING", "1")
-hl.env("SDL_VIDEODRIVER", "wayland")
+hl.env("SDL_VIDEODRIVER", "wayland,x11")
 hl.env("MOZ_ENABLE_WAYLAND", "1")
 
 hl.env("XKB_DEFAULT_LAYOUT", "latam")
@@ -17,5 +17,8 @@ hl.env("XMODIFIERS", "@im=fcitx5")
 hl.env("GTK_THEME", "catppuccin-mocha-blue-standard+default")
 hl.env("GTK_ICON_THEME", "Papirus-Dark")
 hl.env("GTK_APPLICATION_PREFER_DARK_THEME", "1")
+
+hl.env("GNOME_KEYRING_CONTROL", os.getenv("HOME") .. "/.local/share/keyrings")
+hl.env("SSH_AUTH_SOCK", os.getenv("HOME") .. "/.local/share/keyrings/ssh")
 
 hl.env("GRIMBLAST_SCREENSHOTS_DIR", os.getenv("HOME") .. "/Pictures/Screenshots")
